3ee19d397d79880c94abccb6a03c047d02016033
piomin <piotr.minkowski@gmail.com>
2021-06-16 10:17:51 +0200

    change the structure of the project

---
+++ basic-with-db/person-app/src/main/java/pl/redhat/samples/quarkus/person/model/Person.java
@@ -0,0 +1,16 @@
+package pl.redhat.samples.quarkus.person.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Entity
+public class Person extends PanacheEntity {
+    private String name;
+    private int age;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private Integer external;
+}
---
+++ basic-with-db/person-app/src/main/java/pl/redhat/samples/quarkus/person/resource/PersonResource.java
@@ -0,0 +1,38 @@
+package pl.redhat.samples.quarkus.person.resource;
+
+import pl.redhat.samples.quarkus.person.model.Person;
+import pl.redhat.samples.quarkus.person.repository.PersonRepository;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import java.util.List;
+
+@Path("/persons")
+public class PersonResource {
+
+    @Inject
+    PersonRepository personRepository;
+
+    @POST
+    @Transactional
+    public Person addPerson(Person person) {
+        personRepository.persist(person);
+        return person;
+    }
+
+    @GET
+    public List<Person> getPersons() {
+        return personRepository.listAll();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Person getPersonById(@PathParam("id") Long id) {
+        return personRepository.findById(id);
+    }
+
+}
