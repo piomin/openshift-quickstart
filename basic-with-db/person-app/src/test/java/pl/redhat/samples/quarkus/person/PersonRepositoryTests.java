2a5bc754814879caf2bbaa5af1d34c832c5870a0
piomin <piotr.minkowski@gmail.com>
2021-09-02 17:07:37 +0200

    New version of Quarkus + rollback trx test

---
+++ basic-with-db/person-app/src/test/java/pl/redhat/samples/quarkus/person/PersonRepositoryTests.java
@@ -0,0 +1,29 @@
+package pl.redhat.samples.quarkus.person;
+
+import io.quarkus.test.TestTransaction;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pl.redhat.samples.quarkus.person.model.Gender;
+import pl.redhat.samples.quarkus.person.model.Person;
+import pl.redhat.samples.quarkus.person.repository.PersonRepository;
+
+import javax.inject.Inject;
+
+@QuarkusTest
+@TestTransaction
+public class PersonRepositoryTests {
+
+    @Inject
+    PersonRepository personRepository;
+
+    @Test
+    void addPerson() {
+        Person newPerson = new Person();
+        newPerson.age = 22;
+        newPerson.name = "TestNew";
+        newPerson.gender = Gender.FEMALE;
+        personRepository.persist(newPerson);
+        Assertions.assertNotNull(newPerson.id);
+    }
+}
