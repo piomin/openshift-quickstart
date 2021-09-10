98192e16afe6e219a21365a4d32323ec80fcd2c0
piomin <piotr.minkowski@gmail.com>
2021-09-11 00:07:03 +0200

    fix #1 -> `basic-with-db`

--- basic-with-db/person-app/src/main/java/pl/redhat/samples/quarkus/person/repository/PersonRepository.java
+++ basic-with-db/person-app/src/main/java/pl/redhat/samples/quarkus/person/repository/PersonRepository.java
@@ -3,5 +3,8 @@ package pl.redhat.samples.quarkus.person.repository;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import pl.redhat.samples.quarkus.person.model.Person;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class PersonRepository implements PanacheRepository<Person> {
 }
