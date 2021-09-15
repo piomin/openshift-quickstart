2916216cc9b629c1932e7aff52eb368c32c76a4d
piomin <piotr.minkowski@gmail.com>
2021-09-15 14:53:30 +0200

    starting version of `consumer` and `producer` modules

--- event-driven/consumer/src/main/java/pl/redhat/samples/eventdriven/ConsumerAApp.java
+++ event-driven/consumer/src/main/java/pl/redhat/samples/eventdriven/ConsumerAApp.java
@@ -1,28 +1,9 @@
 package pl.redhat.samples.eventdriven;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.config.ListenerContainerCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.listener.*;
-import org.springframework.kafka.retrytopic.RetryTopicConfiguration;
-import org.springframework.kafka.retrytopic.RetryTopicConfigurationBuilder;
-import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.Message;
-import org.springframework.util.backoff.FixedBackOff;
-import pl.redhat.samples.eventdriven.message.CallmeEvent;
-
-import javax.annotation.PostConstruct;
-import java.util.function.Consumer;
 
 @SpringBootApplication
 public class ConsumerAApp {
@@ -32,27 +13,5 @@ public class ConsumerAApp {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerAApp.class, args);
     }
-
-    @Bean
-    public Consumer<Message<CallmeEvent>> eventConsumer() {
-        return event -> {
-            LOG.info("Received: {}", event.getPayload());
-//            throw new UnsupportedOperationException();
-//            Acknowledgment acknowledgment = event.getHeaders().get(KafkaHeaders.ACKNOWLEDGMENT, Acknowledgment.class);
-//            if (acknowledgment != null) {
-//                LOG.info("Manual Ack");
-//                if (event.getPayload().getEventType().equals("COMMIT")) {
-//                    acknowledgment.acknowledge();
-//                    LOG.info("Committed");
-//                }
-//            }
-        };
-    }
-
-    @Bean
-    public ListenerContainerCustomizer<AbstractMessageListenerContainer> containerCustomizer() {
-        return (container, dest, group) -> container.setErrorHandler(new SeekToCurrentErrorHandler(new FixedBackOff(1000, 3)));
-    }
-
 
 }
--- event-driven/producer/src/main/java/pl/redhat/samples/eventdriven/ProducerApp.java
+++ event-driven/producer/src/main/java/pl/redhat/samples/eventdriven/ProducerApp.java
@@ -1,54 +1,14 @@
 package pl.redhat.samples.eventdriven;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.function.context.PollableBean;
-import org.springframework.context.annotation.Bean;
-import pl.redhat.samples.eventdriven.message.CallmeEvent;
-import pl.redhat.samples.eventdriven.message.LargeEvent;
-import reactor.core.publisher.Flux;
-
-import java.util.List;
-import java.util.Random;
-import java.util.function.Supplier;
 
 @SpringBootApplication
 public class ProducerApp {
-
-    private static final Logger LOG = LoggerFactory.getLogger(ProducerApp.class);
-    private static int id = 0;
-    private static final Random RAND = new Random();
 
     public static void main(String[] args) {
         SpringApplication.run(ProducerApp.class, args);
     }
 
-    @Bean
-    public Supplier<CallmeEvent> eventSupplier() {
-        return () -> {
-            int i = RAND.nextInt(8);
-            return new CallmeEvent(++id, "Hello" + id, i == 4 ? "COMMIT" : "PING");
-        };
-    }
-
-//    @Bean
-//    public Supplier<List<CallmeEvent>> eventSupplier() {
-//        return () -> List.of(
-//                new CallmeEvent(++id, "T" + id, "COMMIT"),
-//                new CallmeEvent(++id, "T" + id, "COMMIT"),
-//                new CallmeEvent(++id, "T" + id, "COMMIT"),
-//                new CallmeEvent(++id, "T" + id, "COMMIT"),
-//                new CallmeEvent(++id, "T" + id, "COMMIT"));
-//    }
-
-//    @Bean
-//    public Supplier<LargeEvent> eventSupplier() {
-//        return () -> {
-//            int size = RAND.nextInt(4000);
-//            return new LargeEvent(++id, "PING", 4000 + size);
-//        };
-//    }
 
 }
