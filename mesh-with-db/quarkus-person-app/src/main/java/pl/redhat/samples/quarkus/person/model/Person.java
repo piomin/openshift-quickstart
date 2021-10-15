9e127232d0d999f4dac56fa5bee4544760df35fe
piomin <piotr.minkowski@gmail.com>
2021-10-15 23:12:54 +0200

    update #2

--- mesh-with-db/quarkus-person-app/src/main/java/pl/redhat/samples/quarkus/person/model/Person.java
+++ mesh-with-db/quarkus-person-app/src/main/java/pl/redhat/samples/quarkus/person/model/Person.java
@@ -1,19 +1,49 @@
 package pl.redhat.samples.quarkus.person.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 
-@Data
-@NoArgsConstructor
 @Entity
 public class Person extends PanacheEntity {
     private String name;
     private int age;
     @Enumerated(EnumType.STRING)
     private Gender gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", id=" + id +
+                '}';
+    }
 }
