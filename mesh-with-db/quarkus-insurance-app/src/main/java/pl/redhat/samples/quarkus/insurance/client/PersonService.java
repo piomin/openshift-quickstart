63dbf8a3ed2cfe4d94939f4d3e157bd2af39c743
piomin <piotr.minkowski@gmail.com>
2022-01-13 00:18:32 +0100

    remove headers propagation

--- mesh-with-db/quarkus-insurance-app/src/main/java/pl/redhat/samples/quarkus/insurance/client/PersonService.java
+++ mesh-with-db/quarkus-insurance-app/src/main/java/pl/redhat/samples/quarkus/insurance/client/PersonService.java
@@ -1,6 +1,5 @@
 package pl.redhat.samples.quarkus.insurance.client;
 
-import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import pl.redhat.samples.quarkus.insurance.client.model.Person;
 
@@ -10,7 +9,6 @@ import javax.ws.rs.PathParam;
 
 @Path("/persons")
 @RegisterRestClient
-@RegisterClientHeaders
 public interface PersonService {
 
     @GET
