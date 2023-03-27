54dd86210d7302842702627158a2a5f615a031a9
piomin <piotr.minkowski@gmail.com>
2023-03-27 11:02:10 +0200

    fix errors

--- basic-with-db/insurance-app/src/main/resources/db/changeLog.sql
+++ basic-with-db/insurance-app/src/main/resources/db/changeLog.sql
@@ -14,6 +14,3 @@ insert into insurance(personId, type, amount) values(3, 'LIFE', 30000);
 insert into insurance(personId, type, amount) values(4, 'LIFE', 20000);
 insert into insurance(personId, type, amount) values(5, 'MEDICAL', 1500);
 insert into insurance(personId, type, amount) values(6, 'LIFE', 50000);
-
---changeset piomin:2
-create sequence hibernate_sequence start 1;
