61a8dae25a309f69461d81d5aed1f1e1dd2f286c
piomin <piotr.minkowski@gmail.com>
2023-04-12 16:29:27 +0200

    updates

--- micro-view/department/src/main/java/pl/redhat/samples/department/model/Department.java
+++ micro-view/department/src/main/java/pl/redhat/samples/department/model/Department.java
@@ -1,6 +1,6 @@
 package pl.redhat.samples.department.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
--- micro-view/organization/src/main/java/pl/redhat/samples/organization/model/Organization.java
+++ micro-view/organization/src/main/java/pl/redhat/samples/organization/model/Organization.java
@@ -1,6 +1,6 @@
 package pl.redhat.samples.organization.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
